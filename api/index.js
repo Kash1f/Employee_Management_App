07c5d5755eb10fc60a857a4245468d4a2eab53d5
Kashif <ahmed.kashif1001@gmail.com>
2024-05-05 17:21:29 +0500

    setting up expo router, mongodb, express

---
+++ api/index.js
@@ -0,0 +1,5 @@
+const express = require('expresss');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
+
+// "/"
+//index.js will apear when we open the app as it is the home route
